f27c3ca58e1a973d9d7bade5401c6b04e5245846
Edward Osiychuk <edwardosiychuk@Edwards-Air.lan>
2023-04-24 11:48:34 -0600

    began user question

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //-----------------------Exercise 1-----------------
+            //Your Name: Edd
+            //Your Favorite Color: Black
+            //Your Favorite Animal: 
+            //Your Favorite Band:
+
+            Console.WriteLine("Hello - What is your first name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {userName}. - What is your favorivte color?");
+            var color = Console.ReadLine();
+
+            Console.WriteLine($"{color} is an cool color! What is your favorite animal?");
+            var animal = Console.ReadLine();
+
+            Console.WriteLine("Great! Now, what is yopur favorite band??");
+            var band = Console.ReadLine();
+
         }
     }
 }
